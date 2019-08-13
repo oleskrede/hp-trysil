143defcea974bc1d70d110dae0a4fa9df914f959
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2019-08-14 00:48:28 +0200

    Create basic project setup

---
+++ hp-trysil/src/main/kotlin/no/brightfield/hptrysil/HpTrysilApplication.kt
@@ -0,0 +1,11 @@
+package no.brightfield.hptrysil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HpTrysilApplication
+
+fun main(args: Array<String>) {
+	runApplication<HpTrysilApplication>(*args)
+}
---
+++ hp-trysil/src/main/kotlin/no/brightfield/hptrysil/frontend/HtmlController.kt
@@ -0,0 +1,16 @@
+package no.brightfield.hptrysil.frontend
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun frontpage(model: Model): String {
+        model["title"] = "Harry Potter partay"
+        return "frontpage"
+    }
+}
