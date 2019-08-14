ca7c90a420e092f7341182c368c5ca5c913dc966
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2019-08-14 21:54:01 +0200

    Add empty admin page for point registration

--- hp-trysil/src/main/kotlin/no/brightfield/hptrysil/frontend/AdminController.kt
+++ hp-trysil/src/main/kotlin/no/brightfield/hptrysil/frontend/AdminController.kt
@@ -1,10 +1,18 @@
 package no.brightfield.hptrysil.frontend
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("/admin")
 class AdminController () {
 
+    @GetMapping("/points")
+    fun points(model: Model): String {
+        model["title"] = "Poengregistrering"
+        return "admin_points"
+    }
 }
