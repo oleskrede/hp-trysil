6015cfbe0a8a59f7cb3c7cfcbd0b480d56b940c2
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2019-08-14 21:33:51 +0200

    Add initial points to Gryffindor

--- hp-trysil/src/main/kotlin/no/brightfield/hptrysil/configuration/HpTrysilConfiguration.kt
+++ hp-trysil/src/main/kotlin/no/brightfield/hptrysil/configuration/HpTrysilConfiguration.kt
@@ -2,7 +2,10 @@ package no.brightfield.hptrysil.configuration
 
 import no.brightfield.hptrysil.util.ALL_HOUSES
 import no.brightfield.hptrysil.entity.House
+import no.brightfield.hptrysil.entity.Points
 import no.brightfield.hptrysil.repository.HouseRepository
+import no.brightfield.hptrysil.repository.PointsRepository
+import no.brightfield.hptrysil.util.GRYFFINDOR
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,9 +14,13 @@ import org.springframework.context.annotation.Configuration
 class HpTrysilConfiguration {
 
     @Bean
-    fun databaseInitializer(houseRepository: HouseRepository) = ApplicationRunner {
+    fun databaseInitializer(houseRepository: HouseRepository,
+                            pointsRepository: PointsRepository) = ApplicationRunner {
         ALL_HOUSES.forEach { houseName ->
             houseRepository.save(House(houseName))
         }
+        val gryffindor = houseRepository.findByName(GRYFFINDOR)
+        pointsRepository.save(Points(10, gryffindor!!, "taking a shot"))
+        pointsRepository.save(Points(30, gryffindor!!, "excellent spellcasting"))
     }
 }
---
+++ hp-trysil/src/main/kotlin/no/brightfield/hptrysil/frontend/AdminController.kt
@@ -0,0 +1,10 @@
+package no.brightfield.hptrysil.frontend
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admin")
+class AdminController () {
+
+}
