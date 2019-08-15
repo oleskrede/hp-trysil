cfcf79adbeebce37871ab259a1b9c9271c9d7c0f
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2019-08-15 21:34:00 +0200

    Add Milligram for responsive design and finish mvp of admin point registration page

--- hp-trysil/src/main/kotlin/no/brightfield/hptrysil/frontend/AdminController.kt
+++ hp-trysil/src/main/kotlin/no/brightfield/hptrysil/frontend/AdminController.kt
@@ -3,6 +3,7 @@ package no.brightfield.hptrysil.frontend
 import no.brightfield.hptrysil.entity.Points
 import no.brightfield.hptrysil.repository.HouseRepository
 import no.brightfield.hptrysil.repository.PointsRepository
+import no.brightfield.hptrysil.util.ALL_HOUSES
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -20,24 +21,33 @@ class AdminController (private val houseRepository: HouseRepository,
     @GetMapping("/points")
     fun pointsForm(model: Model): String {
         model["title"] = "Poengregistrering"
+        model["houses"] = ALL_HOUSES
         return "admin_points"
     }
 
     @PostMapping("/points")
     fun pointsSubmit(model: Model,
                      @RequestParam params: Map<String, String>): String {
-        val houseName = params["house"]
+        val houseName = params["houseField"]
                 ?: throw IllegalArgumentException("No house name provided")
-        val points = params["points"]
+        val points = params["pointsField"]?.toInt()
                 ?: throw IllegalArgumentException("No points value provided")
-        val reason = params["reason"]
+        val reason = params["reasonField"]
                 ?: throw IllegalArgumentException("No reason provided")
         val house = houseRepository.findByName(houseName)
                 ?: throw IllegalArgumentException("Wrong house name provided")
 
-        pointsRepository.save(Points(points.toInt(), house, reason))
+        pointsRepository.save(Points(points, house, reason))
 
         model["title"] = "Poengregistrering"
+        model["houses"] = ALL_HOUSES
+        model["saveResult"] = RenderedSaveResult(houseName, points, reason)
         return "admin_points"
     }
+
+    data class RenderedSaveResult(
+            val house: String,
+            val points: Int,
+            val reason: String
+    )
 }
